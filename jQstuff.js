204399adb3c2aa22f96e466b64791c7ff1ac58fd
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-09-05 09:24:47 -0700

    Update jQstuff.js

--- jQstuff.js
+++ jQstuff.js
@@ -1,13 +1,5 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
 $(document).ready(function(){
-  $("#hide").click(function(){
-    $("p").hide();
+  $("h1").click(function(){
+    $("p").toggle();
   });
+});
